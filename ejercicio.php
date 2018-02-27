497a9e676ccdc4c6dffbf36a5a6d2a33c05556ef
Luis felipe <lfsalazar884@misena.edu.co>
2018-02-27 11:44:10 -0500

    definitiva.php

---
+++ ejercicio.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>formulario</title>
+</head>
+<body>
+
+	
+	
+       <h1>calcular la definitiva</h1>
+           
+           <strong> introduzca su nombre</strong>
+
+          <FORM ACTION= "definitiva.php" METHOD= "post">
+        
+          <INPUT TYPE="text"NAME="nombre"><br><br>
+          Nota 1
+          <INPUT TYPE="text"NAME="nota1"><br>
+          Nota 2
+          <INPUT TYPE="text"NAME="nota2"><br>
+          Nota 3
+          <INPUT TYPE="text"NAME="nota3"><br>
+          Nota 4
+          <INPUT TYPE="text"NAME="nota4"><br>
+
+          <INPUT TYPE="submit" value="enviar">
+
+          </FORM>
+      
+
+	  
+
+</body>
+</html>
